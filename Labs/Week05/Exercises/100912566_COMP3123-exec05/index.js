7d7c89ac80bdd1e9e41449f4e94facfbbea6898f
Simon K <Simon@DESKTOP-R9UCS0H>
2024-10-06 20:39:45 -0700

    lab05 updates

--- Labs/Week05/Exercises/100912566_COMP3123-exec05/index.js
+++ Labs/Week05/Exercises/100912566_COMP3123-exec05/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const path = require('path');
 const router = express.Router();
 const bodyParser = require('body-parser');
 
@@ -8,17 +9,16 @@ app.use(bodyParser.json()); // Middleware to parse JSON bodies
 
 // Return home.html page to client
 router.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/home.html'); // Serve the home.html file
+    res.sendFile(__dirname + '/home.html'); // Serve the home.html file
 });
 
 // Return all details from user.json file to client as JSON format
 router.get('/profile', (req, res) => {
-  fs.readFile('user.json', 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).json({ message: 'Error reading user data' });
-    }
-    res.json(JSON.parse(data)); // Return user data as JSON
-  });
+    const filePath = path.join(__dirname, 'user.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        res.writeHead(200, { "Content-Type": "application/json;charset=utf-8"});
+        res.end(data);
+    });
 });
 
 /*
@@ -26,41 +26,41 @@ router.get('/profile', (req, res) => {
 - Read data from user.json file
 */
 router.post('/login', (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  fs.readFile('user.json', 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).json({ message: 'Error reading user data' });
-    }
+    fs.readFile('user.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error reading user data' });
+        }
 
-    const users = JSON.parse(data);
-    const user = users.find(u => u.username === username);
+        const users = JSON.parse(data);
+        const user = users.find(u => u.username === username);
 
-    // - If username is invalid then send response as below 
-    // {
-    //     status: false,
-    //     message: "User Name is invalid"
-    // }
-    if (!user) {
-      return res.json({ status: false, message: "User Name is invalid" });
-    }
-    
-    // - If passsword is invalid then send response as below 
-    // {
-    //     status: false,
-    //     message: "Password is invalid"
-    // }
-    if (user.password !== password) {
-      return res.json({ status: false, message: "Password is invalid" });
-    }
+        // - If username is invalid then send response as below 
+        // {
+        //     status: false,
+        //     message: "User Name is invalid"
+        // }
+        if (!user) {
+            return res.json({ status: false, message: "User Name is invalid" });
+        }
 
-    // - If username and  passsword is valid then send resonse as below 
-    // {
-    //     status: true,
-    //     message: "User Is valid"
-    // }
-    res.json({ status: true, message: "User Is valid" });
-  });
+        // - If passsword is invalid then send response as below 
+        // {
+        //     status: false,
+        //     message: "Password is invalid"
+        // }
+        if (user.password !== password) {
+            return res.json({ status: false, message: "Password is invalid" });
+        }
+
+        // - If username and  passsword is valid then send resonse as below 
+        // {
+        //     status: true,
+        //     message: "User Is valid"
+        // }
+        res.json({ status: true, message: "User Is valid" });
+    });
 });
 
 
@@ -69,8 +69,8 @@ router.post('/login', (req, res) => {
     in HTML format like <b>${username} successfully logout.<b>
 */
 router.get('/logout/:username', (req, res) => {
-  const username = req.params.username;
-  res.send(`<b>${username} successfully logged out.</b>`);
+    const username = req.params.username;
+    res.send(`<b>${username} successfully logged out.</b>`);
 });
 
 
@@ -79,13 +79,14 @@ Add error handling middleware to handle below error
 - Return 500 page with message "Server Error"
 */
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Server Error');
+    console.error(err.stack);
+    res.status(500).send('Server Error');
 });
 
 
 app.use('/', router);
 
+http://localhost:8081
 app.listen(process.env.port || 8081, () => {
-  console.log('Web Server is listening at port ' + (process.env.port || 8081));
+    console.log('Web Server is listening at port ' + (process.env.port || 8081));
 });
